c137d64bf1fcb25972ad18fea76dd480965fa113
Xu Zhaowei <paldier@hotmail.com>
2020-03-21 15:25:06 +0800

    first commit

---
+++ release/src-rt-7.14.114.x/src/linux/linux-2.6.36/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 30 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#30 SMP PREEMPT Wed Jan 8 00:05:00 CST 2020"
+#define LINUX_COMPILE_TIME "00:05:00"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "asus"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.5.3 (Buildroot 2012.02) "
---
+++ release/src/router/rc/merlinr.h
@@ -0,0 +1,59 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ *
+ * Copyright 2019, paldier <paldier@hotmail.com>.
+ * All Rights Reserved.
+ * 
+ *
+ */
+
+
+#ifndef __MERLINR_H__
+#define __MERLINR_H__
+extern void merlinr_init(void);
+extern void merlinr_init_done(void);
+#ifdef RTCONFIG_UUPLUGIN
+extern void exec_uu(void);
+#endif
+#ifdef RTCONFIG_FRS_LIVE_UPDATE
+extern int merlinr_firmware_check_update_main(int argc, char *argv[]);
+#endif
+#if defined(RTCONFIG_SOFTCENTER)
+enum {
+	SOFTCENTER_WAN=1,
+	SOFTCENTER_NAT,
+	SOFTCENTER_MOUNT
+};
+extern void softcenter_eval(int sig);
+#endif
+#if defined(RTAC68U) && !defined(SBRAC1900P)
+extern void tm1900_check(void);
+extern void merlinr_set(const char *name, const char *value);
+extern int merlinr_toolbox(int argc, char **argv);
+#endif
+#if defined(EA6700)
+extern int GetPhyStatus2(int verbose);
+#endif
+#if defined(MERLINR_VER_MAJOR_X) && defined(RTAC86U)
+extern void merlinr_toolbox(void);
+extern void merlinr_patch_nvram(void);
+#elif  defined(MERLINR_VER_MAJOR_X) && defined(RTAC88U)
+extern void merlinr_toolbox(void);
+extern void merlinr_patch_nvram(void);
+#endif
+#endif
+
+
